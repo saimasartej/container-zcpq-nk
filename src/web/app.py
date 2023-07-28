fa70458bb9bb87d485e3f011b6f862856133d92f
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 18:53:29 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,24 +1,15 @@
 import logging
 
 from flask import Flask
-from werkzeug.utils import find_modules, import_string
+
 
 
 def configure_logging():
     # register root logging
     logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('werkzeug').setLevel(logging.INFO)
+    logging.getLogger('logger.log').setLevel(logging.INFO)
 
 
-def register_blueprints(app):
-    """Automagically register all blueprint packages
-    Just take a look in the blueprints directory.
-    """
-    for name in find_modules('blueprints', recursive=True):
-        mod = import_string(name)
-        if hasattr(mod, 'bp'):
-            app.register_blueprint(mod.bp)
-    return None
 
 
 def create_app():
