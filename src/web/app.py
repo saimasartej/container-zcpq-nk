e3127929f70a7faa5935d3fcd05db1c2d94b25d5
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 09:15:08 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -1,23 +1,44 @@
-from flask import Flask, render_template
-from db import get_db, close_db
-import sqlalchemy
-from logger import log
-from requests import Session
-import time
 
-TOKEN = "5955602844:AAFfwmGzOaZoOClIKPtOSLkBjjbVBnpXuGY"
-CHAT_ID = '6093993760'
-SEND_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+import random
+from decimal import Decimal
+# Import flask library.
+from flask import *
+
 app = Flask(__name__)
 
 
+x=2
 
-@app.route("/")
-def index():
+def go():
+
 
 
     while True:
-        time.sleep(60)
-        requests.post(SEND_URL, json={'chat_id': CHAT_ID, 'text': 'nifty_kjhindex_data'})  
-    
-    return "thgrr"
+        global x
+
+        x=random.uniform(20.20, 30.00)
+        x=Decimal(x)
+        x=round(x,2)
+	print x
+        time.sleep(2)
+        
+
+
+@app.route('/')
+def root():
+	return render_template('index.html')
+
+
+@app.route('/_add')
+def add_numbers():
+    a = ["27-04-2017 20:43:11",1,1,7,72.64,49.64,72.12,35.32,28.74,36.18,84.65,19.35,76.15,"0:48:13","141:41:45","72:16:42","0:15:42"]
+
+    a[6]=x
+    return jsonify(a)
+
+
+if __name__ == '__main__':
+
+	
+	thread.start_new_thread(go, ())
+	app.run(host='0.0.0.0', debug=True, threaded=True)
