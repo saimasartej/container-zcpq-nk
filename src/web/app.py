50233bbbc4808f4298c2618deb77524979f40892
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 09:59:09 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -4,6 +4,18 @@ from flask import *
 
 app = Flask(__name__)
 
+x=2
+
+def go():
+    while True:
+        global x
+
+        x=random.uniform(20.20, 30.00)
+        x=Decimal(x)
+        x=round(x,2)
+	print x
+        time.sleep(2)
+
 
 @app.route('/')
 def root():
