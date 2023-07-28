969a24eab13bdb75dbcb19b81f92e8c11270d598
saimasartej <139892532+saimasartej@users.noreply.github.com>
2023-07-28 10:14:28 +0530

    Update app.py

--- src/web/app.py
+++ src/web/app.py
@@ -7,16 +7,7 @@ app = Flask(__name__)
 x=2
 
 def go():
-    while True:
-        global x
-
-        x=random.uniform(20.20, 30.00)
-        x=Decimal(x)
-        x=round(x,2)
-	print x
-        time.sleep(2)
-
-
+	return 0
 @app.route('/')
 def root():
 	return 'index.html'
